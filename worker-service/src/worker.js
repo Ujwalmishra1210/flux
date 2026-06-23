268ac760bebb9f7368c5912440deec92fb9c4476
irony1210 <mishraujwal105@gmail.com>
2026-06-23 17:39:31 +0530

    added notification status lifecycle

--- worker-service/src/worker.js
+++ worker-service/src/worker.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const { Worker } = require("bullmq");
 const IORedis = require("ioredis");
 
+const pool = require("./db/postgres");
+
 const connection = new IORedis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
@@ -12,10 +14,49 @@ const connection = new IORedis({
 const worker = new Worker(
   "notifications",
   async (job) => {
+
+    const notificationId =
+      job.data.notificationId;
+
     console.log(
       "Processing notification:",
-      job.data.notificationId
+      notificationId
     );
+
+    await pool.query(
+      `
+      UPDATE notifications
+      SET status = $1
+      WHERE id = $2
+      `,
+      [
+        "PROCESSING",
+        notificationId
+      ]
+    );
+
+    await new Promise(
+      resolve =>
+        setTimeout(resolve, 3000)
+    );
+
+    await pool.query(
+      `
+      UPDATE notifications
+      SET status = $1
+      WHERE id = $2
+      `,
+      [
+        "SENT",
+        notificationId
+      ]
+    );
+
+    console.log(
+      "Notification sent:",
+      notificationId
+    );
+
   },
   {
     connection,
